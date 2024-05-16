05f21788dc1451cdae558c740148779e0ff9cfb8
Shari <shari.kahl@gmx.de>
2024-05-16 12:21:51 +0200

    versuch1

--- versuch1/src/Receive_Mail.java
+++ versuch1/src/Receive_Mail.java
@@ -1,18 +1,51 @@
 import java.util.Properties;
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Store;
+import javax.mail.*;
 
 public class Receive_Mail {
 	public static void main(String[] args) throws Exception {
 		fetchMail();
 	}
-	
+
 	public static void fetchMail() {
 		try {
-			// your code here
-		} catch(Exception e) {
+			// Initialize mail session
+			Properties properties = new Properties();
+			Session session = Session.getDefaultInstance(properties);
+
+			// Connect to POP3 server
+			Store store = session.getStore("pop3");
+			store.connect("localhost", "labrat", "kn1lab");
+
+			// Open the INBOX folder
+			Folder folder = store.getFolder("INBOX");
+			folder.open(Folder.READ_ONLY);
+
+			// Fetch the messages in the folder
+			Message[] messages = folder.getMessages();
+
+			System.out.println("Log::Messages were fetched successfully\n");
+			System.out.println("[Mails]		" + messages.length);
+
+			// Pretty print the messages in the folder
+			for (var message : messages) {
+				System.out.println("-----------------------------------------------------------");
+				System.out.println("[ID]		" + message.getMessageNumber());
+				System.out.println("[Sender]	" + message.getFrom()[0]);
+				System.out.println("[Subject]	" + message.getSubject());
+				System.out.println("[Sent Date]	" + message.getSentDate().toString());
+				System.out.print("[Body]		" + message.getContent().toString());
+
+				// The last element should have a trailing delimiter (so it looks better)
+				if (message.getMessageNumber() == messages.length) {
+					System.out.println("-----------------------------------------------------------");
+				}
+			}
+
+			folder.close();
+			store.close();
+
+			System.out.println("\nLog::Closed connection.");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
--- versuch1/src/Send_Mail.java
+++ versuch1/src/Send_Mail.java
@@ -1,22 +1,34 @@
+import java.util.Date;
 import java.util.Properties;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage; 
+import javax.mail.internet.MimeMessage;
 
 public class Send_Mail {
-	public static void main(String[] args) {
-		sendMail();   
-	}
-	
-	public static void sendMail() {
-		try {
-			// your code here
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        sendMail();
+    }
+
+    public static void sendMail() {
+        try {
+            // Initialize the mail session
+            Properties properties = new Properties();
+            Session session = Session.getDefaultInstance(properties);
+            MimeMessage msg = new MimeMessage(session);
+
+            // Set all of the neccesary data
+            msg.setFrom("backofen@localhost");
+            msg.setRecipients(Message.RecipientType.TO,
+                    "labrat@localhost");
+            msg.setSubject("Dies ist eine Erinnerung!");
+            msg.setText("Deine Pizza ist jetzt fertig gebacken!");
+            msg.setSentDate(new Date());
+
+            // Send message
+            Transport.send(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
